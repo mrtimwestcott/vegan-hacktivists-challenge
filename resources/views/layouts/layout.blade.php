8e907791d2e8085cd4b4000f52592d1e70ffa05a
Tim (Sarox) <tim@sarox.com.au>
2019-10-04 09:22:50 +1000

    Create template layout

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/css/app.css">
+
+    <title>@yield('title')</title>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -1,41 +1,34 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/css/app.css">
+@extends('layouts.layout')
 
-    <title>Laravel</title>
-<body>
-    <div class="container">
-        <h1>Q&A</h1>
+@section('title', 'Questions')
 
-        <form method="post" action="/questions">
-            @csrf
-            <div class="form-group">
-                <label for="question">Ask a question:</label>
-                <textarea class="form-control" name="question" id="question" required>{{old('question')}}</textarea>
-            </div>
-            @if($errors->any())
-                <div>
-                    <ul>
-                        @foreach($errors->all() as $error)
-                            <li>{{$error}}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
+@section('content')
+    <h1>Q&A</h1>
+
+    <form method="post" action="/questions">
+        @csrf
+        <div class="form-group">
+            <label for="question">Ask a question:</label>
+            <textarea class="form-control" name="question" id="question" required>{{old('question')}}</textarea>
+        </div>
+        @if($errors->any())
             <div>
-                <button class="btn btn-primary" type="submit">Submit</button>
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                </ul>
             </div>
-        </form>
+        @endif
+        <div>
+            <button class="btn btn-primary" type="submit">Submit</button>
+        </div>
+    </form>
 
-        <h2>Browse questions:</h2>
-        <ul class="list-group">
-        @foreach($questions as $question)
-            <li class="list-group-item">{{ $question->question }}</li>
-        @endforeach
-        </ul>
-    </div>
-</body>
-</html>
+    <h2>Browse questions:</h2>
+    <ul class="list-group">
+    @foreach($questions as $question)
+        <li class="list-group-item">{{ $question->question }}</li>
+    @endforeach
+    </ul>
+@endsection
