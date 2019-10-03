3d330cb610674ff1ddb022ac790381ee35678cfb
Tim (Sarox) <tim@sarox.com.au>
2019-10-04 09:18:32 +1000

    Add basic bootstrap styling

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -3,32 +3,39 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/css/app.css">
 
     <title>Laravel</title>
 <body>
-    <h1>Q&A</h1>
-    <ul>
-    @foreach($questions as $question)
-        <li>{{ $question->question }}</li>
-    @endforeach
-    </ul>
-    <form method="post" action="/questions">
-        @csrf
-        <div>
-            <textarea name="question" required>{{old('question')}}</textarea>
-        </div>
-        @if($errors->any())
-            <div>
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li>{{$error}}</li>
-                    @endforeach
-                </ul>
+    <div class="container">
+        <h1>Q&A</h1>
+
+        <form method="post" action="/questions">
+            @csrf
+            <div class="form-group">
+                <label for="question">Ask a question:</label>
+                <textarea class="form-control" name="question" id="question" required>{{old('question')}}</textarea>
             </div>
-        @endif
-        <div>
-            <button type="submit">Submit</button>
-        </div>
-    </form>
+            @if($errors->any())
+                <div>
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <div>
+                <button class="btn btn-primary" type="submit">Submit</button>
+            </div>
+        </form>
+
+        <h2>Browse questions:</h2>
+        <ul class="list-group">
+        @foreach($questions as $question)
+            <li class="list-group-item">{{ $question->question }}</li>
+        @endforeach
+        </ul>
+    </div>
 </body>
 </html>
