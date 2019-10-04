42f9482e88f154dc679960965c4b199c22fe30cc
Tim (Sarox) <tim@sarox.com.au>
2019-10-04 11:39:46 +1000

    Sort questions by newest first

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -7,11 +7,24 @@ use App\Question;
 
 class QuestionController extends Controller
 {
+    /**
+     * Displays a list of all questions sorted by newest first
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index() {
-        $questions = Question::all();
+        $questions = Question::all()->sortByDesc(function($question) {
+            return $question->created_at;
+        });
         return view('questions.index', compact('questions'));
     }
 
+    /**
+     * Stores the question and redirects to the list of questions
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function store(Request $request) {
         $request->validate([
            'question' => ['required','min:5','regex:/\?$/'],
@@ -20,6 +33,12 @@ class QuestionController extends Controller
         return redirect('/questions');
     }
 
+    /**
+     * Displays a question with all answers
+     *
+     * @param Question $question
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show(Question $question) {
         return view('questions.show', compact('question'));
     }
